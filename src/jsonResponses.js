eda814d802b229159fa5b32a5bea76991964cd9f
Paul Wilson <pjw6900@rit.edu>
2019-02-14 23:28:23 -0500

    first commit

---
+++ src/jsonResponses.js
@@ -0,0 +1,68 @@
+const users = {};
+
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+const getUsers = (request, response) => {
+  const responseJson = {
+    users,
+  };
+
+  respondJSON(request, response, 200, responseJson);
+};
+
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required',
+  };
+
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+};
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found',
+    id: 'notFound',
+  };
+
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+const notFoundMeta = (request, response) => respondJSONMeta(request, response, 404);
+
+module.exports.addUser = addUser;
+module.exports.getUsers = getUsers;
+module.exports.getUsersMeta = getUsersMeta;
+module.exports.notFound = notFound;
+module.exports.notFoundMeta = notFoundMeta;
